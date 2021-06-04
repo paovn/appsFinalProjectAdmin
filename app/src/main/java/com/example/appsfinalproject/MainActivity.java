713e8b3a77a96fbb754a16aeac0495da0f8241b0
Daniel Fernández <47828494+7yrionLannister@users.noreply.github.com>
2021-06-04 00:10:19 -0500

    Added main screen with fragment container and principal fragment

--- app/src/main/java/com/example/appsfinalproject/MainActivity.java
+++ app/src/main/java/com/example/appsfinalproject/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.appsfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigator;
+    private ProductFragment productFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productFragment = ProductFragment.newInstance();
+
+        requestPermissions();
+        configureNavigator();
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, 12345);
+    }
+
+    private void configureNavigator() {
+        navigator = findViewById(R.id.navBarAdmin);
+
+        navigator.setOnNavigationItemSelectedListener(
+                (menuItem)->{
+                    switch(menuItem.getItemId()){
+                        case R.id.principalItem:
+                            showFragment(productFragment);
+                            break;
+                    }
+                    return true; // le estoy diciendo que si estoy manejando la acción de la barra
+                }
+        );
+    }
+
+    private void showFragment(Fragment fragment){
+        // todas las actividades vienen con el fragmentManager solo lo debemos llamar
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainerAdmin, fragment);
+        transaction.commit();
     }
 }
