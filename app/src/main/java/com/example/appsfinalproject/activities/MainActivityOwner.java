87c5d3743b435b0761404d8cce74ce7a96b39f3f
Carlos <carlos.restrepo5@correo.icesi.edu.co>
2021-06-06 15:16:19 -0500

    Compilation error due to the < button texts solved

--- app/src/main/java/com/example/appsfinalproject/activities/MainActivityOwner.java
+++ app/src/main/java/com/example/appsfinalproject/activities/MainActivityOwner.java
@@ -17,6 +17,7 @@ public class MainActivityOwner extends AppCompatActivity {
 
     private ShopFragment shopFragment;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
