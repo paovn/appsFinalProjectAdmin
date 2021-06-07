5386f899d3d2cd3b21424da532d0b3813d9dfbec
nel2424 <nelsonquinones2424@gmail.com>
2021-06-07 13:18:47 -0500

    Modificación del modelo para realizar queries a la base de datos acerca del modulo de contabilidad, modificación de la interfaz para añadir el precio de un producto

--- app/src/main/java/com/example/appsfinalproject/model/ContabilidadLocal.java
+++ app/src/main/java/com/example/appsfinalproject/model/ContabilidadLocal.java
@@ -1,12 +1,15 @@
 package com.example.appsfinalproject.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ContabilidadLocal {
     private ArrayList<RegistroContable> registros;
+    private String id;
 
-    public ContabilidadLocal(ArrayList<RegistroContable> registros) {
-        this.registros = registros;
+    public ContabilidadLocal(String id) {
+        this.registros = new ArrayList<>();
+        this.id = id;
     }
 
     public ArrayList<RegistroContable> getRegistros() {
@@ -16,4 +19,14 @@ public class ContabilidadLocal {
     public void setRegistros(ArrayList<RegistroContable> registros) {
         this.registros = registros;
     }
+
+    public void addRegistro(String nombre, Date fecha, double costo, Tipo_registro tipo, String id) {
+        RegistroContable reg = new RegistroContable(nombre,fecha,costo,tipo,id);
+        registros.add(reg);
+    }
+
+    public String getId() {
+        return id;
+    }
+
 }
