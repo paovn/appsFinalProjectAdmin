2907923c1291054bd8ea25d9a6407da02556744f
nel2424 <nelsonquinones2424@gmail.com>
2021-06-13 10:15:08 -0500

    Avances en la funcionalidad de graficación de contabilidad, se visualizan los dias

--- app/src/main/java/com/example/appsfinalproject/fragments/owner/LocalStatsFragment.java
+++ app/src/main/java/com/example/appsfinalproject/fragments/owner/LocalStatsFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.appsfinalproject.R;
@@ -17,7 +19,6 @@ import com.example.appsfinalproject.model.ContabilidadLocal;
 import com.example.appsfinalproject.model.Local;
 import com.example.appsfinalproject.model.RegistroContable;
 import com.example.appsfinalproject.model.Tipo_registro;
-import com.example.appsfinalproject.model.Usuario;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -36,11 +37,21 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
     private TextView periodTV;
     private Button back;
     private Button next;
+    private Button generateChart;
+    private EditText cantidadPeriodoET;
+    private CheckBox ingresosCB;
+    private CheckBox egresosCB;
+    private CheckBox utilidadCB;
     private LineChart chart;
     private FirebaseFirestore db;
     private FirebaseAuth auth;
+    private ContabilidadLocal contabilidad;
+    private int rangeDateType;
+    private String messageRangeType[];
 
-    public final int MILI_SEC_DAY = 86400000;
+    public static final int DAY_PERIOD = 0;
+    public static final int WEEK_PERIOD = 1;
+    public static final int MONTH_PERIOD = 2;
 
     public LocalStatsFragment() {
         // Required empty public constructor
@@ -61,24 +72,51 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_local_stats, container, false);
-        periodTV = v.findViewById(R.id.period);
+        periodTV = v.findViewById(R.id.period_TV);
+        cantidadPeriodoET = v.findViewById(R.id.catidadPeriodo_ET);
+        ingresosCB = v.findViewById(R.id.ingresos_CB);
+        egresosCB = v.findViewById(R.id.egresos_CB);
+        utilidadCB = v.findViewById(R.id.utilidad_CB);
         back = v.findViewById(R.id.back_period_button);
         back.setOnClickListener(this);
         next = v.findViewById(R.id.advance_period_button);
         next.setOnClickListener(this);
-
+        generateChart = v.findViewById(R.id.generateChart_BTN);
+        generateChart.setOnClickListener(this);
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
-
         updateLocalAccounting();
-
         chart = v.findViewById(R.id.linechart);
-        loadData(30);
-
+        messageRangeType = new String[]{"Días", "Semanas", "Meses"};
+        rangeDateType = DAY_PERIOD;
+        loadData();
         return v;
     }
 
-    private void loadData(int days) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back_period_button:
+                rangeDateType = (rangeDateType-1)%3;
+                if(rangeDateType == -1) {
+                    rangeDateType = 2;
+                }
+                periodTV.setText(messageRangeType[rangeDateType]);
+                break;
+            case R.id.advance_period_button:
+                rangeDateType = (rangeDateType+1)%3;
+                periodTV.setText(messageRangeType[rangeDateType]);
+                break;
+            case R.id.generateChart_BTN:
+                String periodo_raw = cantidadPeriodoET.getText().toString();
+                int periodo = Integer.parseInt(periodo_raw);
+                selectData(periodo,rangeDateType);
+                break;
+        }
+
+    }
+
+    private void loadData() {
         String id = auth.getCurrentUser().getUid();
         db.collection("users").whereEqualTo("id", auth.getCurrentUser().getUid()).get().addOnSuccessListener(
                 command -> {
@@ -89,9 +127,8 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
                             command1 -> {
                                 Log.e(">>>", "Antes del out of bounds el id del local es: " + idLocal);
                                 Local local = command1.getDocuments().get(0).toObject(Local.class);
-                                ContabilidadLocal contabilidad = local.getContabilidad();
-                                ArrayList data = contabilidad.getRange(days);
-                                updateChart(data);
+                                contabilidad = local.getContabilidad();
+                                selectData(30, DAY_PERIOD);
                             }
                     ).addOnFailureListener(
                             command2 ->{
@@ -106,29 +143,33 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
         );
     }
 
-    private void updateChart(ArrayList<Entry> data) {
-        /*ArrayList<Integer> x_axis = new ArrayList<>();
-        for (int i = 0; i < ; i++) {
+    public void selectData(int periods,int periodType){
+        ArrayList data = new ArrayList();
+        ArrayList xAxis = new ArrayList();
 
-        }*/
+        switch (periodType){
+            case DAY_PERIOD:
+                data = contabilidad.getRangeDays(periods);
+                break;
+            case WEEK_PERIOD:
+                break;
+            case MONTH_PERIOD:
+                break;
+        }
+
+        updateChart(data);
+    }
+
+    private void updateChart(ArrayList<Entry> data) {
         this.getActivity().runOnUiThread(
                 () -> {
                     LineDataSet lineDataSet = new LineDataSet(data,"Rendimientos");
                     LineData dataChart = new LineData(lineDataSet);
                     chart.setData(dataChart);
+                    chart.invalidate();
                 }
         );
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.back_period_button:
-                break;
-            case R.id.advance_period_button:
-                break;
-        }
     }
 
     //Creación de información dummy
@@ -143,7 +184,7 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
                             command1 -> {
                                 Log.e(">>>", "Antes del out of bounds el id del local es: " + idLocal);
                                 Local local = command1.getDocuments().get(0).toObject(Local.class);
-                                if(local.getContabilidad() == null){
+                                if(local.getContabilidad() == null || local.getContabilidad().getRegistros().size() == 0){
                                     ContabilidadLocal contabilidad1 = new ContabilidadLocal(UUID.randomUUID().toString());
                                     createContabilidadData(contabilidad1);
                                     local.setContabilidad(contabilidad1);
@@ -171,8 +212,11 @@ public class LocalStatsFragment extends Fragment implements View.OnClickListener
         long todayMiliSeconds = currentDate.getTime();
         ArrayList<RegistroContable> registros = new ArrayList<>();
         for (int i = 0; i < days; i++) {
-            registros.add(new RegistroContable("Registro "+String.valueOf(i*2),new Date(todayMiliSeconds+MILI_SEC_DAY*i),rand.nextDouble()*1000000, Tipo_registro.EGRESO,UUID.randomUUID().toString()));
-            registros.add(new RegistroContable("Registro "+String.valueOf(i*2+1),new Date(todayMiliSeconds+MILI_SEC_DAY*i),rand.nextDouble()*1000000, Tipo_registro.INGRESO,UUID.randomUUID().toString()));
+            double select =rand.nextDouble();
+            if ( select > 0.5) {
+                registros.add(new RegistroContable("Registro " + String.valueOf(i * 2), new Date(todayMiliSeconds + ContabilidadLocal.MILI_SEC_DAY * i), rand.nextDouble() * 1000000, Tipo_registro.EGRESO, UUID.randomUUID().toString()));
+                registros.add(new RegistroContable("Registro " + String.valueOf(i * 2 + 1), new Date(todayMiliSeconds + ContabilidadLocal.MILI_SEC_DAY * i), rand.nextDouble() * 1000000, Tipo_registro.INGRESO, UUID.randomUUID().toString()));
+            }
         }
         contabilidad1.setRegistros(registros);
     }
